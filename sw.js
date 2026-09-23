bbc8965a9873e148e5648519babcdc5ce2ba79bd
Candie-gitbuh <candie.portejoie@gmail.com>
2026-09-23 17:16:41 +0200

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = "ma-cuisine-v4-2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icône-192.png",
+  "./icône-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
